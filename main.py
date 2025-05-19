1e1d0a52d2dac35d1ab8c26c745937e20ccde934
William McAllister <you@example.com>
2025-05-19 10:39:49 -0400

    Fix: use dynamic port for Render

--- main.py
+++ main.py
@@ -356,5 +356,6 @@ def matchups():
 
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
