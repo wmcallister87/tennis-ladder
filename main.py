d650b6d236ccfef32712eb208ebd99ce431ad057
William McAllister <you@example.com>
2025-05-19 18:59:54 -0400

    adding main coding

--- main.py
+++ main.py
@@ -359,6 +359,8 @@ def debug_db():
     return f"Using: {app.config['SQLALCHEMY_DATABASE_URI']}"
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
