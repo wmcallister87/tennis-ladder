d4f27f6629c71aa14cefdb140ec4aff59b76f7c6
William McAllister <you@example.com>
2025-05-19 16:39:57 -0400

    Swith to PostgreSQL using Database_Url

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ print(">>> Running correct main.py")
 app = Flask(__name__)
 app.secret_key = "your-secret-key"  # Replace this for production
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ladder.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
